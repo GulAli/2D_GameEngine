329ca8de9f5e78521f1b15a8df81bf7a761fc9d1
Ali <a.aligul@hotmail.com>
2023-08-21 09:59:35 +0300

    Delete build directory

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: F:/GameDevelopment/(3) 2D_GameEngine - Tilemaps
-# Build directory: F:/GameDevelopment/(3) 2D_GameEngine - Tilemaps/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
